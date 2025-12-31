a24104745b4e53d027577984578640627c5d3b62
William Timothy Tin O <williamtimothytino2004@gmail.com>
2025-12-31 23:13:02 +0000

    making a Endpoint Health
    Maps HTTP GET requests
    Listens at path /health
    Combined with /api  to make  /api/health so when someone sends a get request to /api/health -- run this method

---
+++ currencyconverter/controller/HealthController.java
@@ -0,0 +1,22 @@
+package com.example.currencyconverter.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+/*marks this class as a Spring-managed component by telling Spring that this class handles
+HTTP requests , Methods inside return data not HTML pages 
+
+Combines @controller and @ResponseBody
+
+Everything returned from this class is sent directly back to the client as a response
+What happens WITHOUT @RestController
+.  Spring will not detect the class
+.  Your endpoints will never exist
+.  Requests to /api/health would return 404
+*/
+
+@RestController
+@RequestMapping("/api")
+public class HealthController {
+
+
+}

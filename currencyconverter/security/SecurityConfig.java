906a0f3af0e40699b1eb9adcb1661c82f4b57f28
William Timothy Tin O <williamtimothytino2004@gmail.com>
2026-01-01 21:54:20 +0000

    Update SecurityConfig.java

--- currencyconverter/security/SecurityConfig.java
+++ currencyconverter/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.example.currencyconverter.security;
+package com.example.currencyconverter;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;

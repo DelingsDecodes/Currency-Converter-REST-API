19a956576c65818e6b1c39debb7744563bd3f691
William Timothy Tin O <williamtimothytino2004@gmail.com>
2026-01-09 22:41:38 +0000

    Update CurrencyService.java

--- currencyconverter/service/CurrencyService.java
+++ currencyconverter/service/CurrencyService.java
@@ -1,7 +1,14 @@
 package com.example.currencyconverter.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class CurrencyService {
+
+    private final WebClient webClient;
+
+    public CurrencyService() {
+        this.webClient = WebClient.create("https://api.frankfurter.app");
+    }
 }
